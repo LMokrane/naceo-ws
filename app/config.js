bfcb6f04df6c7ac91928beaee5fcc7d83227dc0c
Mokrane LATEB <mokrane.lateb@sqy.fr>
2022-08-16 00:15:51 +0200

    mongodb url ds config

--- app/config.js
+++ app/config.js
@@ -1,7 +1,11 @@
 const config = {
   host: "demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self",
   port: 9001,
-  path: "./logs"
+  path: "./logs",
+  mongodb: {
+    host: "localhost",
+    port: 27017
+  }
 };
 
 export default config;
--- app/index.js
+++ app/index.js
@@ -4,12 +4,13 @@ import WebSocket, { createWebSocketStream } from 'ws';
 import config from './config.js';
 
 import mongodb from './mongodb.js';
+const urlMongodb = `${ config.mongodb.host }:${ config.mongodb.port }`;
 
 const wss = new WebSocket(`wss://${ config.host }`);
 const messageStream = createWebSocketStream(wss, { encoding: 'utf8' });
 
 async function run() {
-  const Message = await mongodb('172.17.0.3:27017');
+  const Message = await mongodb(urlMongodb);
   for await (let data of messageStream) {
     const message = new Message({ log: data });
     await message.save();
