71906bf5e5c538dadd13153eeea44059d80a4f51
Mokrane LATEB <mokrane.lateb@sqy.fr>
2022-08-16 21:43:27 +0200

    change path des logs et ip container docker mongodb

--- app/config.js
+++ app/config.js
@@ -1,9 +1,9 @@
 const config = {
   host: "demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self",
   port: 9001,
-  path: "/vagrant/app",
+  path: "./logs",
   mongodb: {
-    host: "localhost",
+    host: "172.17.0.3",
     port: 27017
   }
 };
