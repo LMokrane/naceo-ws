c2e7e3ceba317ebc227bffc031f95f764478fc1b
Mokrane LATEB <mokrane.lateb@sqy.fr>
2022-09-02 21:08:38 +0200

    suppr lignes locale inutiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # Configuration clavier français
 echo "Clavier francais"
-rm /etc/locale.gen
-touch /etc/locale.gen
-echo "fr_FR.UTF-8 UTF-8" > /etc/locale.gen
-locale-gen
 echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
 echo "KEYMAP=fr-latin1" > /etc/vconsole.conf
 loadkeys fr
