888e0acb5fbf000d6a1c798a28fb433e2fb5131e
Mokrane LATEB <mokrane.lateb@sqy.fr>
2022-09-02 20:40:12 +0200

    fix port number + add mongodb dbname in config

--- app/config.js
+++ app/config.js
@@ -1,8 +1,9 @@
 const config = {
   host: "demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self",
-  port: 9001,
+  port: 443,
   path: "./logs",
   mongodb: {
+    dbname: "naceo-ws",
     host: "192.168.56.5",
     port: 27017
   }
